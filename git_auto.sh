8912ea1bf70558e1f65dfaed52b8ff6015b6347d
Naveen Kumar S <imacenaveen@gmail.com>
2019-11-16 14:33:06 +0530

    $1

---
+++ git_auto.sh
@@ -0,0 +1,6 @@
+cd /home/naveen/Desktop/Python_Files/ESP/MicroPython
+git remote remove origin 
+git remote add origin https://imacenaveen:Snk!1990@github.com/imacenaveen/MicroPython.git
+git add .
+git commit -m '$1'
+git push --set-upstream origin master
